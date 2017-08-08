16f8acc79819e520a6f3650411c30e656f083d3a
Jonathan Chaney <jonchaney@gmail.com>
2017-08-07 22:31:59 -0700

    profile updated

--- frontend/app/components/home/home.js
+++ frontend/app/components/home/home.js
@@ -81,21 +81,43 @@ const styles = StyleSheet.create({
     top: 30,
     fontSize: 22,
   },
+profileContainer: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
   userDisplay: {
     color: '#F2cFB1',
+    fontFamily: 'Kohinoor Bangla',
+    fontSize: 25,
+    margin: 8,
   },
   xrpDisplay: {
     color: '#F2cFB1', 
+    fontFamily: 'Kohinoor Bangla',
+    fontSize: 25,
+    margin: 8,  
   },
   xrpOptionsContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
+    bottom: 50,
+    alignSelf: 'auto',
   },
   xrpOption: {
     fontFamily: 'Copperplate',
-    fontSize: 20,
-    color: '#F2cFB1',
+    fontSize: 22,
+    color: '#C29436',
+    margin: 15,
+    borderRadius: 4,
+    borderWidth: 1.9,
+    borderColor: '#F2cFB1',
+    padding: 9,
+    flex: 1,
+    shadowOpacity: 0.4,
+    textAlign: 'center',
+    
   }
 });
 
