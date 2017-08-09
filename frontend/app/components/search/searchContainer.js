fd43f3463ce2863c3be6f3ba3452afe10d585f70
Alex Milbert <alex.milbert@gmail.com>
2017-08-08 20:16:35 -0700

    search display

---
+++ frontend/app/components/search/searchContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
