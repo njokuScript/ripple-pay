127408d9a265e26d6e1021d168365f10be8bb143
Jonathan Chaney <jonchaney@gmail.com>
2017-08-09 18:31:23 -0700

    basic deposit styling

---
+++ frontend/app/components/wallet/walletContainer.js
@@ -0,0 +1,15 @@
+import{ connect } from 'react-redux';
+import Wallet from './wallet';
+
+const mapStateToProps = ({}) => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+mapStateToProps,
+mapDispatchToProps
+)(Wallet);
