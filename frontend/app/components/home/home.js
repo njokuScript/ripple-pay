ce4ef8decb80145bc815bc93e91a1b402c7c5da2
Jonathan Chaney <jonchaney@gmail.com>
2017-08-12 15:58:31 -0700

    home container

--- frontend/app/components/home/home.js
+++ frontend/app/components/home/home.js
@@ -19,16 +19,37 @@ class Home extends React.Component {
     super(props);
     this.onLogout = this.onLogout.bind(this);
     this.state = {page: 'cloud'};
+    this.displayTransactions = this.displayTransactions.bind(this);
   }
 
   onLogout() {
     this.props.unauthUser();
   }
 
+  displayTransactions() {
+    if (!this.props.transactions.length === 0) {
+      const transactions = this.props.transactions.map(function(transaction, idx) {
+        return (
+          <Text style={styles.transactions}key={idx}>{transaction}</Text>
+        );
+      });
 
-  //After the component has mounted with 0 for balance and and [] for transactions, we go to the database
-  //with this thunk action creator to make sure this is indeed the same or if there are transactions or a balance or if not.
+      return (
+        <View>
+          {transactions}
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text style={styles.transactions}>no transactions</Text>
+        </View>
+      );
+    }
+  }
 
+  // After the component has mounted with 0 for balance and and [] for transactions, we go to the database
+  // with this thunk action creator to make sure this is indeed the same or if there are transactions or a balance or if not.
   componentDidMount() {
     this.props.requestTransactions(this.props.user);
   }
@@ -54,41 +75,44 @@ class Home extends React.Component {
       title: "Send",
       component: SendContainer,
       navigationBarHidden: true
-    })
+    });
   }
 
   render()
   {
     return (
       <View style={styles.mainContainer}>
+        <View style={styles.topContainer}>
+          <View style={styles.logoContainer}>
+            <Text style={styles.logo}>
+              ripplePay
+            </Text>
+          </View>
+        </View>
+        <View>
+            <View style={styles.balanceContainer}>
+              <Text style={styles.balance}>
+                  {this.props.balance} XRP
+              </Text>
+            </View>
+        </View>
+          <View style={styles.transactionsContainer}>
+              {this.displayTransactions()}
+          </View>
         <Tabs selected={this.state.page} style={{backgroundColor:'white'}}
-             onSelect={el=>this.setState({page:el.props.name})}>
-          <TouchableOpacity>
-              <Text>Home</Text>
+            onSelect={el=>this.setState({page:el.props.name})}>
+        <TouchableOpacity>
+            <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity name="source" onPress={this.navSearch.bind(this)} >
+          <Text>Search</Text></TouchableOpacity>
+          <TouchableOpacity name="pool" onPress={this.navWallet.bind(this)}>
+            <Text>Deposit</Text>
           </TouchableOpacity>
-          <TouchableOpacity name="source" onPress={this.navSearch.bind(this)} >
-            <Text>Search</Text></TouchableOpacity>
-            <TouchableOpacity name="pool" onPress={this.navWallet.bind(this)}>
-              <Text>Deposit</Text>
-            </TouchableOpacity>
-          <TouchableOpacity name="Stream" onPress={this.navSend.bind(this)}>
-            <Text>Send</Text>
-            </TouchableOpacity>
-      </Tabs>
-        {/* <Text style={styles.welcome}>
-            Welcome to Ripple Pay
-        </Text>
-        <Text style={styles.instructions}>
-            Balance: 10 XRP
-        </Text>
-        <Text style={styles.instructions}>
-            Selected page: {this.state.page}
-        </Text> */}
-        <View style={styles.navContainer}>
-          <TouchableOpacity onPress={this.onLogout}>
-            <Text>logout</Text>
+        <TouchableOpacity name="Stream" onPress={this.navSend.bind(this)}>
+          <Text>Send</Text>
           </TouchableOpacity>
-       </View>
+        </Tabs> 
       </View>
     );
   }
@@ -100,19 +124,56 @@ const styles = StyleSheet.create({
   mainContainer: {
      flex: 1,
      justifyContent: 'center',
-     alignItems: 'center',
-     backgroundColor: '#335B7B',
+     flexDirection: 'column',
+     backgroundColor: 'white'
    },
-   welcome: {
-     fontSize: 20,
+  topContainer: {
+    alignItems: 'center',
+    backgroundColor: '#335B7B'
+  },
+  logoContainer: {
+    flex: 1,
+    paddingBottom: 10,
+    backgroundColor: '#335B7B'
+  },
+  logo: {
+    textAlign: 'center',
+    marginTop: 25,
+    color: 'white',
+    fontSize: 15
+  },
+  balanceContainer: {
+    flex: 1,
+    marginTop: 10,
+    width: 350,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 3,
+    shadowOpacity: 1.0
+  },
+   balance: {
+     flex: 1,
      textAlign: 'center',
-     margin: 10,
+     fontSize: 25,
+     color: 'black',
    },
-   instructions: {
-     textAlign: 'center',
-     marginBottom: 5,
-     fontSize: 15
-   },
+    transactionsContainer: {
+      flex: 1,
+      borderTopWidth: 1,
+      borderColor: 'white',
+      marginTop: 25,
+      paddingTop: 10,
+      paddingLeft: 10
+    },
+    transactions: {
+      flex: 1,
+      color: 'white'
+    }
 });
 
 export default Home;
