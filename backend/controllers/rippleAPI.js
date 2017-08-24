fdcc7311e1e36cfa862eab3030a25a5fffa98cc5
Jonathan Chaney <jonchaney@gmail.com>
2017-08-23 23:12:55 -0700

    rippled server

--- backend/controllers/rippleAPI.js
+++ backend/controllers/rippleAPI.js
@@ -5,7 +5,10 @@ const { addresses, bank } = require('../controllers/addresses');
 class Rippled {
   constructor(){
       this.api = new RippleAPI({
-        server: 'wss://s2.ripple.com' // Public rippled server hosted by Ripple, Inc.
+        // server: 'wss://s2.ripple.com' // Public rippled server hosted by Ripple, Inc.
+        server: 'ws://54.70.149.103'
+        // proxy: 'http://ec2-54-70-149-103.us-west-2.compute.amazonaws.com'
+        // key: '-----BEGIN CERTIFICATE-----MIIEoQIBAAKCAQEAv++k/TOdg5bLcBJBxl9xEqpHhwV40AT1oYG/Fpb4nAFKzxEGtXEoFhTa8hWQoo3Tsqq62cHQy3+lq8FxKJv3ElYe/WRh39XLt4IBA4EVUyfPfICAHN1SMYXQ6PW3Knq0W2ulhUZTxgU7uS/y6S6Je/UY+dk/2+743GCAeyo7jJHjfjYHQuH2pWnMIddqgw2tNMwXjSqwofC8R+RIj6G9JlK0PI72JjmH/R0p8jzQCs8J0j/Ztos5ZW8PuDDdcUhGbpoBNhYpHphuKu6bJvdU5SMc8H5WihDKF5JiR3KLNd6taAWQ7ai/KgwFZpaFnx/+fKz3r9MRGBuZpwuh4t8sawIDAQABAoIBAH8pqsoGlGlGUuqhbeqb+TJAlrsiKPTJFGQ4rf2tcHELeiDOSv+TRNe8YYLaPsUz6foJm1oCSu9IVt8AfpRFIOCJJmcDRsUhxKKEA1/q+dHqIlF+YWK+T2J4Ifz2LrB6M1wOac9u1LnxZaw/1FQhfP0fWbjO8rwoC8EWPolbZbCvH+UHmKW9zsrg25glI8NgTv6BjLaXqrj+JlkGs49V9NZ9BnDMilop4ON18hONhUrBM1bWvBelH1IJjcfCoYSdTSTQvL3Pfw+hWhH039f3co2AD/TtYUcnmbVAwdi6nNGrmCdd9JAjZkJcLv6qVZ/emT1Afo0KHOcy0PDPjQjfLsECgYEA93LstLolenNFIKJ8DUkiWHuyHbNjRFjFHNeRa7Jc3Ty+9aleruvZ60XMebvT00bgLrlEmBNPLd2vWFrtYxWL5Zhjfe3XAtWv6bN7sxPfe06L+0jlEMybuBfJdklnSynkPBV6k83Da6NTfejYIHtaKUGojhSwsNqWrTgBAHR7WakCgYEAxpGfIhvuPk725zSLneOyewGYIRfNN4B4A5Rgd4+NWJZK3ZmoEHl7WiG1K0NQDfTIl0VahFFqdrL49GodPczTgN/Y+Jh6CFzDYwd04H/RV/2iobUy1flm0hssqYZm0PFZl3ygkXP+wkO0lTM2MrR6/uFLhKvBvhJc5sCXOaMlKfMCgYBHjbUDAbzqrsBzkmi60sYavhIgYoo27lB5aaCEevw9gV5a3VIi1NKcJP/ex8CWHK2g9TPvoc3Ino0gyJJSj513k9xZQthk2nPW6W7AtuNt3aW+0TJj7DavQjmgedNjucZk1XUvnlV/H6h6BUXSBrloqrLZ+wOo8FyX1FT907EDmQJ/C6J8sILycrQtw15Qz++Hha2SPF4oqBfe8FbBUEAZCBfGSzW1o0F6+M0A0IghKLAvEK3/n8Ele88Ax1FwZS6bNfHnxKkqfVLyH9tP/dd/bKhv3wm+8MqsO6Nb+pLlc1iw5y9mOR+Szc8pxZQ6mL36dvS8eAmYUIrC5sJiGf51LwKBgQCQBEJUvmUhF33mrnlIdm/M1Rt0OEC1Z5uDgz8DhJMHRa0IGSseXLmtO+8gBzmCrKala5SKfI5mLVLYQP/7SGAsty1EdKCcsdFLMlKHoRDxgI1K5LmLboncXcATrXqYRm0Zn41H5cZzlYy6UT4nQxTS8a4WTW+G7IX+5I7z9v6RUQ==-----END CERTIFICATE-----'
         //Need to change this to a private one later.
       });
       this.api.on('error', (errorCode, errorMessage) => {
@@ -142,10 +145,10 @@ class Rippled {
 }
 
 
-// let server = new Rippled();
+let server = new Rippled();
 // server.connect().then(() => server.signAndSend("rs1DXnp8LiKzFWER8JrDkMA7xBxQy1KrWi", "r4QDfkUkpNSkuo4m4SnfxgDbrryrtTn883", "shm8TtYiTHiHn7FaqFjZgYQTqkFP6", 3, 1234, 1466900933));
 // let address = "r9bxkP88S17EudmfbgdZsegEaaM76pHiW6";
-// server.connect().then(() => server.getSuccessfulTransactions("rs1DXnp8LiKzFWER8JrDkMA7xBxQy1KrWi"));
+server.connect().then(() => server.getSuccessfulTransactions("rs1DXnp8LiKzFWER8JrDkMA7xBxQy1KrWi"));
 // server.connect().then(() => console.log(server.api.generateAddress()));
 
 module.exports = Rippled;
