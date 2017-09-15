042170b4a1f316a58ab45af2728078fcc7b55714
Jonathan Chaney <jonchaney@gmail.com>
2017-09-14 22:56:38 -0700

    remove tabs from send to user in-app and add back arrow

--- frontend/app/components/banksend/banksend.js
+++ frontend/app/components/banksend/banksend.js
@@ -13,7 +13,7 @@ import {
 } from 'react-native';
 import Tabs from 'react-native-tabs';
 import Button from 'react-native-buttons';
-import Icon from 'react-native-vector-icons/Octicons';
+import Icon from 'react-native-vector-icons/Entypo';
 
 // create a component
 
@@ -31,23 +31,23 @@ class BankSend extends Component {
   //MAKE SURE TO LEAVE THIS HERE AND THEN ADD YOUR TABS
   //WE HAVE TO REQUEST TRANSACTIONS EVERY TIME WE GO TO THE WALLET OR THE HOME.
   //Make sure to request Transactions BEFORE you request address and dest tag before you go to the wallet.
-  navWallet() {
+  // navWallet() {
     // this.props.requestTransactions(this.props.user);
     // this.props.requestAddressAndDesTag(this.props.user.user_id);
-    this.props.navigator.push({
-      title: 'Wallet',
-      component: WalletContainer,
-      navigationBarHidden: true
-    });
-  }
+  //   this.props.navigator.push({
+  //     title: 'Wallet',
+  //     component: WalletContainer,
+  //     navigationBarHidden: true
+  //   });
+  // }
 
-  navExchange() {
-    this.props.navigator.push({
-      title: "Exchange",
-      component: ExchangeContainer,
-      navigationBarHidden: true
-    });
-  }
+  // navExchange() {
+  //   this.props.navigator.push({
+  //     title: "Exchange",
+  //     component: ExchangeContainer,
+  //     navigationBarHidden: true
+  //   });
+  // }
 
   navSearch() {
     this.props.navigator.push({
@@ -58,14 +58,14 @@ class BankSend extends Component {
   }
   //I am not required to do request transactions here because this will happen automatically from componentDidMount in home.js
 
-  navHome() {
-    // this.props.requestTransactions(this.props.user);
-    this.props.navigator.push({
-      title: 'Home',
-      component: HomeContainer,
-      navigationBarHidden: true
-    });
-  }
+  // navHome() {
+  //   // this.props.requestTransactions(this.props.user);
+  //   this.props.navigator.push({
+  //     title: 'Home',
+  //     component: HomeContainer,
+  //     navigationBarHidden: true
+  //   });
+  // }
 
   sendPayment(){
     this.setState({disabled: true});
@@ -75,6 +75,9 @@ class BankSend extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <TouchableOpacity onPress={this.navSearch.bind(this)} >
+          <Icon name="chevron-left" size={30} color="white" />
+        </TouchableOpacity>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>
             Sending to {this.props.otherUser}
@@ -101,20 +104,6 @@ class BankSend extends Component {
             </Text>
           </TouchableOpacity>
         </View>
-        <Tabs selected={this.state.page} style={{backgroundColor:'white'}}>
-            <TouchableOpacity name="cloud" onPress={this.navHome.bind(this)}>
-              <Text>Home</Text>
-            </TouchableOpacity>
-            <TouchableOpacity name="source" onPress={this.navSearch.bind(this)}>
-              <Text>Search</Text>
-            </TouchableOpacity>
-            <TouchableOpacity name="pool" onPress={this.navWallet.bind(this)}>
-              <Text>Wallets</Text>
-            </TouchableOpacity>
-            <TouchableOpacity name="Stream" onPress={this.navExchange.bind(this)}>
-              <Text>Exchange</Text>
-            </TouchableOpacity>
-       </Tabs>
       </View>
     );
   }
@@ -135,7 +124,6 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: '#335B7B',
   },
-
   titleContainer: {
     padding: 10,
     alignItems: 'center',
@@ -200,159 +188,5 @@ const styles = StyleSheet.create({
   }
 });
 
-//make this component available to the app
+// make this component available to the app
 export default BankSend;
-
-
-
-
-
-
-// import React from 'react';
-// import HomeContainer from '../home/homeContainer';
-// import SearchContainer from '../search/searchContainer';
-// import WalletContainer from '../wallet/walletContainer';
-//
-// import { View,
-//   Text,
-//   StyleSheet,
-//   TouchableOpacity,
-//   Image,
-//   Dimensions,
-//   TextInput } from 'react-native';
-// import Tabs from 'react-native-tabs';
-// import Button from 'react-native-buttons';
-//
-//
-// class Send extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {page: 'stream'};
-//   }
-//
-// navHome() {
-//   this.props.navigator.push({
-//     title: 'Home',
-//     component: HomeContainer,
-//     navigationBarHidden: true
-//   });
-// }
-//
-// navSearch() {
-//   this.props.navigator.push({
-//     title: 'Search',
-//     component: SearchContainer,
-//     navigationBarHidden: true
-//   });
-// }
-//
-// navWallet() {
-//   this.props.navigator.push({
-//     title: 'Wallet',
-//     component: WalletContainer,
-//     navigationBarHidden: true
-//   });
-// }
-//
-// sendRipple() {
-//   return;
-// }
-//
-// requestRipple() {
-//   return;
-// }
-//
-//
-//
-//   render()
-//  {
-//    return (
-//      <View style={styles.mainContainer}>
-//        <View style={styles.buttonContainer}>
-//          <Button style={styles.button} onPress={this.sendRipple.bind(this)}>Send</Button>
-//          <Button style={styles.button} onPress={this.requestRipple.bind(this)}>Request</Button>
-//        </View>
-//        <Tabs selected={this.state.page} style={{backgroundColor:'white'}}
-//            onSelect={el=>this.setState({page:el.props.name})}>
-//
-//          <TouchableOpacity name="cloud" onPress={this.navHome.bind(this)}>
-//            <Text>Home</Text>
-//         </TouchableOpacity>
-//
-//           <TouchableOpacity name="source" onPress={this.navSearch.bind(this)}>
-//           <Text>Search</Text>
-//          </TouchableOpacity>
-//
-//            <TouchableOpacity name="pool" onPress={this.navWallet.bind(this)}>
-//              <Text>Deposit</Text>
-//            </TouchableOpacity>
-//
-//            <TouchableOpacity>
-//               <Text>Send</Text>
-//           </TouchableOpacity>
-//      </Tabs>
-//
-//       <Text style={styles.welcome}>
-//         Stream - Send your Ripple
-//      </Text>
-//       <Text style={styles.instructions}>
-//          Selected page: {this.state.page}
-//      </Text>
-//      </View>
-//    );
-//  }}
-//
-//  const {width, height} = Dimensions.get('window');
-//  const styles=StyleSheet.create({
-//    mainContainer: {
-//       flex: 1,
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       backgroundColor: '#335B7B',
-//     },
-//     buttonContainer: { //Needs proper flexing from jon
-//
-//       flexDirection: 'row',
-//       // margin: 8
-//     },
-//     button: {
-//
-//       justifyContent: 'space-around',
-//       padding: 14,
-//       margin: 7,
-//       backgroundColor: '#E8C25E',
-//       borderRadius: 6,
-//
-//     },
-//    welcome: {
-//      fontSize: 20,
-//      textAlign: 'center',
-//      margin: 10,
-//    },
-//     title: {
-//       color: 'white',
-//       fontSize: 20,
-//       justifyContent: 'center',
-//       fontFamily: 'Kohinoor Bangla',
-//     },
-//     inputContainer: {
-//       padding: 5,
-//       margin: 10,
-//       borderWidth: 2,
-//       borderRadius: 10,
-//       borderColor: "white",
-//       backgroundColor: 'white'
-//     },
-//     input: {
-//       height: 26,
-//       backgroundColor: 'white'
-//     },
-//     instructions: {
-//       textAlign: 'center',
-//       color: '#333333',
-//       marginBottom: 5,
-//       fontSize: 15
-//     }
-//  });
-//
-//  export default Send;
