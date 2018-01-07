edb4f52156f7453194e96b49540dd767772bed35
devanshpatel <dev@cloudcade.com>
2018-01-06 19:01:29 -0800

    changes

--- frontend/app/components/home/home.js
+++ frontend/app/components/home/home.js
@@ -232,7 +232,7 @@ class Home extends React.Component {
         {this.displayTransactions()}
       </ScrollView>
       <CustomButton
-        performAction="Load next 25 Transactions"
+        performAction="Load next 10 Transactions"
         buttonColor="white"
         isDisabled={false}
         handlePress={ this.state.shapeshift ? this.loadNextShapeShiftTransactions : this.loadNextTransactions }
