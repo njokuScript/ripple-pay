798f4743b5d67d1375b7551e49a0510582211e4b
Jonathan Chaney <jonchaney@gmail.com>
2018-01-15 19:20:28 -0800

    settungs tab added

---
+++ frontend/app/components/settings/settingsContainer.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Settings from './settings';
+import { requestUsers } from '../../actions';
+
+const mapStateToProps = ({ user }) => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Settings);
