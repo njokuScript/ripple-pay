9766f2b94dfdcf8de873b97fb5d3b2142e0a0499
devanshpatel <dev@cloudcade.com>
2018-01-26 02:17:02 -0800

    change backend for login from 1 device

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.use(bodyParser.json());
 
 app.use('/v1', rateLimit.apiLimiter);
 // make token through middleware for everything except signup, which must create user first.
-app.use(/^(?!\/v1\/(signup))/, mung.json(
+app.use(/^(?!\/v1\/(signup|endsession))/, mung.json(
   function transform(body, req, res) {
     body.token = Token.tokenForUser(req.user);
     return body;
