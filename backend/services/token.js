893d1eeaca28fbac8a01689b7dbe87b3f0521e55
devanshpatel <dev@cloudcade.com>
2018-02-04 19:59:07 -0800

    merge in separate repo changelly stuff backend

--- backend/services/token.js
+++ backend/services/token.js
@@ -1,23 +1,27 @@
 const jwt = require('jwt-simple');
 const Redis = require('../services/redis');
 
-let secret;
+let secret, redisTokenExpiry;
 
 if (process.env.NODE_ENV=='production') {
     secret = process.env.SECRET;
     // making 30 mins for now, but change to 3 later
-    exports.EXPIRE_TIME = 10000;
+    exports.EXPIRE_TIME = 30*60*1000;
+    
+    redisTokenExpiry = exports.EXPIRE_TIME/1000;
+
 } else {
     secret = require('../configs/config').SECRET;
 
-    exports.EXPIRE_TIME = 10000000;
+    exports.EXPIRE_TIME = 100000000;
+    redisTokenExpiry = 1;
+
 }
 // could this be problematic in different countries??
 exports.tokenForUser = function (user) {
     let timeStamp = new Date().getTime();
     let expireStamp = new Date(timeStamp + exports.EXPIRE_TIME).getTime();
-    const secondsExpiry = exports.EXPIRE_TIME/1000;
-    Redis.setInCache("logged-in", user.id, true, secondsExpiry);
+    Redis.setInCache("logged-in", user.id, true, redisTokenExpiry);
 
     const token = jwt.encode({
         sub: user.id,
