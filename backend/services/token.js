4e426462d196500c1024525f50aef1f75d49b7ea
Jonathan Chaney <jonchaney@gmail.com>
2018-02-07 13:34:27 -0800

    merge

--- backend/services/token.js
+++ backend/services/token.js
@@ -7,8 +7,9 @@ if (process.env.NODE_ENV=='production') {
     secret = process.env.SECRET;
     // making 30 mins for now, but change to 3 later
     exports.EXPIRE_TIME = 30*60*1000;
-    
-    redisTokenExpiry = exports.EXPIRE_TIME/1000;
+    // this needs to be fixed later!
+    // redisTokenExpiry = exports.EXPIRE_TIME/1000;
+    redisTokenExpiry = 1;
 
 } else {
     secret = require('../configs/config').SECRET;
