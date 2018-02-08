6b269c47b8447b9c86fc33a003daf6d872302ac7
Jonathan Chaney <jonchaney@gmail.com>
2018-02-08 14:08:30 -0800

    personal wallet qrCode styled

--- frontend/app/components/wallet/personalWallet.js
+++ frontend/app/components/wallet/personalWallet.js
@@ -31,8 +31,6 @@ class PersonalWallet extends React.Component {
         });
     }
 
-    //We have to disable the button so they can't generate more than 5 desTags
-
     setSecret(secret) {
         this.setState({
             personalSecret: secret
@@ -65,28 +63,29 @@ class PersonalWallet extends React.Component {
         const disabled = this.state.disabled;
         if (this.props.personalAddress) {
             const imageSource = Util.getQRCodeSource(this.props.personalAddress);
+            console.log({uri: imageSource});
             return (
                 <View style={styles.walletDisplay}>
-
                     <View style={styles.imageContainer}>
                         <TouchableOpacity style={styles.image} underlayColor='#111F61' onPress={() => this.clipBoardCopy(this.props.personalAddress)}>
+                            <View style={styles.qrBackground}>
                             <Image
                                 style={styles.qrCode}
                                 source={{ uri: imageSource }}
                             />
+                            </View>
                             <View>
-                                <Text style={styles.walletFont}>{this.props.personalAddress}</Text>
+                                <Text style={styles.addressFont}>{this.props.personalAddress}</Text>
                             </View>
                         </TouchableOpacity>
                     </View>
-
                     <View>
                         <View style={styles.destTag}>
                             {this.state.personalSecret ? <Text style={styles.walletFont}>secret key: {this.state.personalSecret}</Text> : null}
                             <Text style={styles.walletFont}>Note: RipplePay does not store your Secret Key so please store it. You will need it to make payments.</Text>
-                    <TouchableOpacity disabled={disabled} onPress={this.removePersonalAddress}>
-                        <Text style={disabled ? styles.redButton : styles.greenButton}>Remove Personal Wallet</Text>
-                    </TouchableOpacity>
+                            <TouchableOpacity disabled={disabled} onPress={this.removePersonalAddress}>
+                                <Text style={disabled ? styles.redButton : styles.greenButton}>Remove Personal Wallet</Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </View>
@@ -122,13 +121,40 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#111F61',
     },
-    buttonsContainer: {
+    imageContainer: {
         flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
+        paddingTop: height / 25,
+        alignItems: 'center',
+        backgroundColor: '#111F61'
+    },
+    image: {
+        flex: 1,
+        alignItems: "center",
+    },
+    qrCode: {
+        width: height / 4,
+        height: height / 4,
+    },
+    qrBackground: {
+        borderColor: 'white',
+        borderWidth: 25
+
+    },
+    walletDisplay: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    noWalletsButtonsContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         flexDirection: 'column',
-        marginLeft: 35,
-        marginRight: 35,
+    },
+    addressFont: {
+        color: 'white',
+        fontSize: height / 50,
+        textAlign: 'center',
+        marginTop: height / 50
     },
     redButton: {
         fontFamily: 'Kohinoor Bangla',
@@ -151,97 +177,6 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: 'white',
         fontSize: 15,
-    },
-    destintro: {
-        color: 'white',
-        fontSize: 20
-    },
-    walletsContainer: {
-        flex: -1,
-        flexDirection: 'column',
-        marginTop: 190,
-        width: 330,
-        marginLeft: 20,
-    },
-    walletAddress: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 15,
-    },
-    walletintro: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 20
-    },
-    noWalletsButtonsContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-    },
-    destTag: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        top: -300,
-        padding: 15
-    },
-    wallets: {
-        flex: 1,
-        fontFamily: 'Kohinoor Bangla',
-    },
-    walletFont: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 15,
-    },
-    cashRegister: {
-        marginTop: 10,
-        color: 'white',
-        fontSize: 16,
-    },
-    address: {
-        flex: -1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        width: 370,
-    },
-    clipBoardContainer: {
-        borderColor: 'white',
-        borderWidth: 1,
-        padding: 1,
-        borderRadius: 3,
-        backgroundColor: 'white'
-    },
-    wallet: {
-        flex: -1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderColor: 'white',
-        borderBottomWidth: 1,
-        paddingBottom: 10,
-        width: 330,
-        marginTop: 10,
-    },
-    imageContainer: {
-        flex: 1,
-        paddingTop: height / 25,
-        alignItems: 'center',
-        backgroundColor: '#111F61',
-    },
-    image: {
-        flex: 1,
-        alignItems: "center"
-    },
-    qrCode: {
-        width: height / 3,
-        height: height / 3,
-    },
-    walletDisplay: {
-        flex: 1,
-        flexDirection: "column",
     },
 });
 
